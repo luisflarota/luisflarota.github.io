bfffec11d545b4fe47e2412f6709c198697e427f
luisflarota <fernando.larota@gmail.com>
2025-01-19 13:24:32 -0800

    chore: added the nextconfig file

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+    basePath: "",
+    assetPrefix: "",
+  }
+  
+  module.exports = nextConfig
+  
+  
